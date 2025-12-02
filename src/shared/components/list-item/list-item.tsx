840ed928822799da871b64639b71f4d7a439a934
Vitor Moutim <moutimg@gmail.com>
2025-12-02 13:43:39 -0300

    fix: correcao dos itens do menu

--- src/shared/components/list-item/list-item.tsx
+++ src/shared/components/list-item/list-item.tsx
@@ -1,13 +1,40 @@
 import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function ListItem(
   { icon, text, activeColor = '#e8006f', route }:
-  { icon: string, text: string, activeColor?: string, route?: string }) {
+    { icon: string, text: string, activeColor?: string, route?: string }) {
   const router = useRouter();
   const pathname = usePathname();
-  console.log(pathname);
-  
-  const isActive = route === '/' ? pathname === '/' : pathname === route || pathname.startsWith(route + '/');
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (!route) {
+      setIsActive(false);
+      return;
+    }
+
+    if (route === '/') {
+      if (pathname === '/' || /^\/\d+$/.test(pathname)) {
+        setIsActive(true);
+        return;
+      }
+      setIsActive(false);
+      return;
+    }
+
+    if (route !== '/') {
+      const routeBase = route.replace(/s$/, ''); // /days -> /day
+      
+      if (pathname.startsWith(route) || pathname.startsWith(routeBase)) {
+        setIsActive(true);
+        return;
+      }
+    }
+
+    setIsActive(false);
+  }, [pathname, route]);
+
   const handleRedirect = (route: string) => router.push(route);
 
   return (
